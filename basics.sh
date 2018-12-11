19595254257544cd64b39b1af22291256be4fcf8
Mrinal Sinha <themrinalsinha@gmail.com>
2018-12-11 12:20:26 +0530

    getting user inputs

--- basics.sh
+++ basics.sh
@@ -4,7 +4,18 @@ echo 'Hello world'  # printing string to terminal.
 
 # variables
 NAME='Mrinal'             # prefer to use upper case variable name.
-echo 'Hello $NAME'        # simply calling the variable to string.
+echo "Hello $NAME"        # simply calling the variable to string.
 GAME='Foot'
 echo "I like ${GAME}ball" # It will concate the variable to the string.
 
+# user inputs and comments
+read -p 'What is your name: ' NAME
+echo "Hello $NAME, how are you ?"
+
+echo "Please, enter name and age: "
+read NAME AGE
+echo "Hello $NAME, you are $AGE years old."
+
+echo "Please enter your full name: "
+read FNAME LNAME
+echo "Your name is $FNAME $LNAME"
