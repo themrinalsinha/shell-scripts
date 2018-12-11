cc53459407f668b1b0cbcd210026d84158a35009
Mrinal Sinha <themrinalsinha@gmail.com>
2018-12-11 12:15:29 +0530

    variable assigning and printing to terminal

---
+++ basics.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# comment line
+echo 'Hello world'  # printing string to terminal.
+
+# variables
+NAME='Mrinal'             # prefer to use upper case variable name.
+echo 'Hello $NAME'        # simply calling the variable to string.
+GAME='Foot'
+echo "I like ${GAME}ball" # It will concate the variable to the string.
+
